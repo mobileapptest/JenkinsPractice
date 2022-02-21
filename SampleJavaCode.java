8dac7e9eda0e1667fabe9bbbf2d74b1bcf1df338
mobileapptest <97483551+mobileapptest@users.noreply.github.com>
2022-02-21 15:50:50 -0600

    Initial commit

---
+++ SampleJavaCode.java
@@ -0,0 +1,15 @@
+/**
+ * Jenkins Practice Sample Java Program
+ *
+ */
+public class SampleJavaCode 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is sample Jenkins Practice" );
+        System.out.println( "to communicate thru jenkins with java Program" );
+        System.out.println( "Created a new java sample program to print some messages" );
+      
+              
+    }
+}
